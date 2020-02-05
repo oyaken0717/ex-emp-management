afed508e2904fa74dc3965ef3ae98d282ccbf7aa
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-05 16:04:55 +0900

    【add】AdministratorControllerクラスにloginメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,11 @@
 package jp.co.sample.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +24,9 @@ import jp.co.sample.service.AdministratorService;
 @RequestMapping("")
 public class AdministratorController {
 
+	@Autowired
+	private HttpSession session;
+	
 	@Autowired
 	AdministratorService administratorService;
 	
@@ -82,5 +88,27 @@ public class AdministratorController {
 	public String toLogin() {
 		return "administrator/login";
 	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
+		if (administrator==null) {
+			model.addAttribute("エラー", "メールアドレスまたはパスワードが不正です");
+		}else {
+			session.setAttribute("administratorName", administrator);
+		}
+		return "forward:/eployee/showList";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
